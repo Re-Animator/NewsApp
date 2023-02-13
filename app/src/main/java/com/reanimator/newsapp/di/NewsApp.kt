4c6fdc0b9483768f7f62cda7d39c21e3fbf7e1cd
Re-Animator <reanimator1904@gmail.com>
2023-02-13 19:46:41 +0400

    adapter, repo, download data from api

---
+++ app/src/main/java/com/reanimator/newsapp/di/NewsApp.kt
@@ -0,0 +1,7 @@
+package com.reanimator.newsapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApp(): Application()
--- app/src/main/java/com/reanimator/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/reanimator/newsapp/ui/main/MainFragment.kt
@@ -1,18 +1,29 @@
 package com.reanimator.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.reanimator.newsapp.R
 import com.reanimator.newsapp.databinding.FragmentDetailsBinding
 import com.reanimator.newsapp.databinding.FragmentMainBinding
+import com.reanimator.newsapp.ui.adapters.NewsAddapter
+import com.reanimator.newsapp.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val mBinding get() = _binding!!
+
+    private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAddapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +31,37 @@ class MainFragment : Fragment() {
     ): View? {
         _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { responce ->
+            when(responce) {
+                is Resource.Success -> {
+                    progress_bar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    progress_bar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("checkData", "MainFragment: error: ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    progress_bar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAddapter()
+        news_adapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
